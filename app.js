fc9f634ae1f556d1368b264f277839181dcd59ae
FauziN825 <FauziFaruq.Nabbani@mitrais.com>
2021-06-21 20:31:20 +0700

    add error handling

--- app.js
+++ app.js
@@ -17,4 +17,12 @@ const itemRouter = require('./src/items')
 app.use('/items',itemRouter)
 
 
+app.all('*', (req, res) => {
+    res.status(404).send("Error Not Found");
+});
+
+app.use( function(err, req, res, next){
+    res.status(500).send( err.message );
+} );
+
 app.listen(port, () => console.log(`listening on port 3000`))
